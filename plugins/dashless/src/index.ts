c2d93a7f9a8536748de9eab866d98ff1a8be4c99
Awesomegamergame <68134172+Awesomegamergame@users.noreply.github.com>
2024-08-07 11:35:06 -0400

    Fixed Dashless because no one else did

---
+++ plugins/dashless/src/index.ts
@@ -0,0 +1,50 @@
+import { ReactNative } from "@vendetta/metro/common";
+import { after } from "@vendetta/patcher";
+import { logger } from "@vendetta";
+
+const { View } = ReactNative;
+
+const log = (message: string, data?: any) => {
+  logger.log(`[Dashless] ${message}`, data);
+};
+
+// Function to recursively traverse and modify children
+const traverseAndModify = (node) => {
+  if (typeof node === "string") {
+    return node.replace(/-/g, " ");
+  }
+
+  if (Array.isArray(node)) {
+    return node.map(traverseAndModify);
+  }
+
+  if (node && typeof node === "object" && node.props && node.props.children) {
+    return {
+      ...node,
+      props: {
+        ...node.props,
+        children: traverseAndModify(node.props.children),
+      },
+    };
+  }
+
+  return node;
+};
+
+const patchRender = () => {
+  return after("render", View, (_, res) => {
+    log("View render patched");
+
+    const modifiedTree = traverseAndModify(res);
+
+    log("React tree modified");
+
+    return modifiedTree;
+  });
+};
+
+const unpatchRender = patchRender();
+
+export const onUnload = () => {
+  unpatchRender();
+};
